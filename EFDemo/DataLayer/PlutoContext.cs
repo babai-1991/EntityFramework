3561cdefe70e9f150fea117c8536f81bd06ad8a6
Suvankar Das <iambabai0@gmail.com>
2021-04-18 22:11:18 +0530

    Ef Code first with new database

---
+++ EFDemo/DataLayer/PlutoContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using EFDemo.DomainModel;
+
+namespace EFDemo.DataLayer
+{
+    public class PlutoContext : DbContext
+    {
+        public PlutoContext():base("name=MyPlutoDatabase")
+        {
+            
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
